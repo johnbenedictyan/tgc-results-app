b19d598829c47717ec53e171002c27e7d3ee8eb1
John Benedict <johnbenedictyan@gmail.com>
2022-04-06 17:23:23 +0800

    Added sentry.

--- next.config.js
+++ next.config.js
@@ -1,11 +1,17 @@
 /** @type {import('next').NextConfig} */
+const {
+    withSentryConfig
+} = require('@sentry/nextjs')
+
 const withPWA = require('next-pwa')
 const runtimeCaching = require('next-pwa/cache')
 
-module.exports = withPWA({
-  pwa: {
-    dest: 'public',
-    runtimeCaching,
-  },
-  compress: false,
+module.exports = withSentryConfig(withPWA({
+    pwa: {
+        dest: 'public',
+        runtimeCaching,
+    },
+    compress: false,
+}), {
+
 })
