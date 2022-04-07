27b40c0edd0021c89c99cda05ea90b95edcd8134
John Benedict <johnbenedictyan@gmail.com>
2022-04-07 19:16:18 +0800

    Updated the logic of the login api.

--- pages/api/users/login.ts
+++ pages/api/users/login.ts
@@ -17,18 +17,19 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
                 let user = await userModel.findOne({ email }).exec();
                 if (!user) {
                     return res.json({ status: 401, code: "unauthorized" });
-                }
-                try {
-                    user.comparePassword(password, (err: Error, isMatch: boolean) => {
-                        if (isMatch) {
-                            const token = jwt.sign({ email: user.email, role: user.role }, JWT_SECRET);
-                            return res.send({ status: 200, token: token });
-                        } else {
-                            return res.json({ status: 401, code: "unauthorized" });
-                        }
-                    });
-                } catch (err) {
-                    return res.json({ status: 400, success: false });
+                } else {
+                    try {
+                        user.comparePassword(password, (err: Error, isMatch: boolean) => {
+                            if (isMatch) {
+                                const token = jwt.sign({ email: user.email, role: user.role }, JWT_SECRET);
+                                return res.send({ status: 200, token: token });
+                            } else {
+                                return res.json({ status: 401, code: "unauthorized" });
+                            }
+                        });
+                    } catch (err) {
+                        return res.json({ status: 400, success: false });
+                    }
                 }
             } catch (err) {
                 return res.json({ status: 400, success: false });
