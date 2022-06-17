0085bf79a274dfca78d20acded24c07872408213
kunxin-chor <chorkunxin@yahoo.com>
2022-06-17 08:58:23 +0000

    fixed admin handler to account for Bearer in front of JWT

--- lib/adminHandler.ts
+++ lib/adminHandler.ts
@@ -5,8 +5,8 @@ import { JWT_SECRET } from "../util/secrets";
 function adminHandler(req: NextApiRequest, res: NextApiResponse) {
     if(!req.headers.authorization) {
         return res.status(401).json({ status: "error", code: "unauthorized" });
-    } else{
-        let token = req.headers.authorization;
+    } else{     
+        let token = req.headers.authorization.split(' ')[1];
         jwt.verify(token, JWT_SECRET, (err, decoded: any) => {
             if(!decoded.role || decoded.role != 'ADMIN') {
                 return res.status(401).json({ status: "error", code: "unauthorized" });
--- pages/api/tutorials/[id].ts
+++ pages/api/tutorials/[id].ts
@@ -23,7 +23,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
                 res.json(tutorial);
             }
             break;
-        case 'PUT':
+        case 'PUT':            
             tutorial = await tutorialModel.findOneAndUpdate({ _id: req.query.id }, req.body);
             if (tutorial === null) {
                 res.status(404);
